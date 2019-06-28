1fc7f056b7daebfb62b2dfd490f12ea5894cb043
Adrian Popan <adrian.popan@gmail.com>
2019-06-28 01:08:19 -0500

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -34,7 +34,7 @@ class Song
     def self.genre_count
         genre_count = {}
         @@genres.each do |genre|
-            if genre_count[genre]
+            if genre_count[genre] = true
                 genre_count[genre] += 1 
             else
                 genre_count[genre] = 1
@@ -46,7 +46,7 @@ class Song
     def self.artist_count
         artist_count = {}
         @@artists.each do |artist|
-            if artist_count[artist]
+            if artist_count[artist] = true
                 artist_count[artist] +=1
             else
                 artist_count[artist] = 1
